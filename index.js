d5a5298d06fef4663a1ac819b8650f8a6238320b
Ivan <ivanburgomistrenko@gmail.com>
2022-12-27 22:02:22 +0200

    add del func

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// const arr = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
-// const numbers = arr.filter(item => typeof item === 'number');
-// function sum(numbers) {
-//   let s = 0;
-//   for (let i = 0; i < numbers.length; i++) {
-//     s += numbers[i];
-//   }
-//   return (s / numbers.length);
-// }
-// console.log(sum(numbers));
+const arr = [5, 66, 'pool', 7, 99, null, { name: 'Sam' }, [], true, 78];
+const numbers = arr.filter(item => typeof item === 'number');
+function sum(numbers) {
+  let s = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    s += numbers[i];
+  }
+  return (s / numbers.length);
+}
+console.log(sum(numbers));
 
-// let x = prompt('x');
-// let y = prompt('y');
-// let znak = prompt('znak');
+let x = prompt('x');
+let y = prompt('y');
+let znak = prompt('znak');
 
-// const calc = (x, y, znak) => {
-//   switch (znak) {
-//     case '+':
-//       return x + y;
-//     case '-':
-//       return x - y;
-//     case '/':
-//       return x / y;
-//     case '*':
-//       return x * y;
-//     case '%':
-//       return x % y;
-//     case '^':
-//       return x ** y;
-//     default: console.log('sorry');
-//   }
-// };
-// console.log(calc(+x, +y, znak));
+const calc = (x, y, znak) => {
+  switch (znak) {
+    case '+':
+      return x + y;
+    case '-':
+      return x - y;
+    case '/':
+      return x / y;
+    case '*':
+      return x * y;
+    case '%':
+      return x % y;
+    case '^':
+      return x ** y;
+    default: console.log('sorry');
+  }
+};
+console.log(calc(+x, +y, znak));
 
 let M = prompt('Длина первого масива?');
 let array = [];
@@ -50,3 +50,20 @@ function double() {
   console.log(array);
 }
 double();
+
+let l = [];
+let str = prompt('Що буде в рядку?');
+let elemDel = prompt('скільки елементів треба прибрати?');
+
+for (i = 0; i < elemDel; i++) {
+  l[i] = prompt('Введіть елемент');
+}
+
+function show(str, l) {
+  for (i = 0; i < l.length; i++) {
+    console.log(l[i]);
+    str = str.split(`${l[i]}`).join('');
+  }
+  console.log(str);
+}
+show(str, l);
